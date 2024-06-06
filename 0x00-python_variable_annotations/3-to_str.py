af2b1cd0cd9317a31bb84dcc8065c68232c345a5
adiomi90 <adiomi90@outlook.com>
2024-06-06 15:02:28 +0300

    to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""this script returns the to_str of a function"""
+
+
+def to_str(n: float) -> str:
+    """_summary_
+
+    Args:
+        n (float): _description_
+
+    Returns:
+        str: _description_
+    """
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
+    return str(n)
