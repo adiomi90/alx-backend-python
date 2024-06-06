f5c7900b1c044ccbbdc0b5ca4df5bad962ca12c9
adiomi90 <adiomi90@outlook.com>
2024-06-06 13:57:58 +0300

    concat two strings"

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"This script concat two string"
+
+
+def concat(str1: str, str2: str) -> str:
+    """_summary_
+
+    Args:
+        str1 (str): _description_
+        str2 (str): _description_
+
+    Returns:
+        str: _description_
+    """
+    return str1 + str2
