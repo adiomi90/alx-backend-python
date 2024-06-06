ccc2a73a3eb68b3be87b5d5b106fdc989b8f0ae1
adiomi90 <adiomi90@outlook.com>
2024-06-06 14:41:01 +0300

    math floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import math
+
+
+def floor(n: float) -> float:
+    """_summary_
+
+    Args:
+        n (float): _description_
+
+    Returns:
+        float: _description_
+    """
+    return math.floor(n)
