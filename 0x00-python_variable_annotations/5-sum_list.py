d4189cd03c4efd58feead586782237324062b099
adiomi90 <adiomi90@outlook.com>
2024-06-06 17:53:32 +0300

    list of float

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""function for functions that sums"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """_summary_
+
+    Args:
+        input_list (float): _description_
+
+    Returns:
+        float: _description_
+    """
+    return sum(input_list)
