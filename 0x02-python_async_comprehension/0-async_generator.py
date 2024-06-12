7509eb9d0927c5684e7358bf71b9e8d9b21fedcb
adiomi90 <adiomi90@outlook.com>
2024-06-12 03:59:02 +0300

    python async await

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,13 +2,14 @@
 """ This code create async generator """
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """_summary_
 
     Yields:
-        _type_: _description_
+        Generator[float, None, None]: _description_
     """
     for _ in range(10):
         await asyncio.sleep(1)
